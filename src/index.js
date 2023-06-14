430cb0f2c344c5b4447fcbc443d323a05a4ecd30
Ana <104568665+Lazy020@users.noreply.github.com>
2023-06-14 14:41:37 +0000

    ...

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const { getDatabaseInstance } = require("./database")
 
 const app = express()
 
-app.use(express.static(__dirname + '/../public'));
+app.use(express.static(__dirname + '/../public'))
 
 // CRUD
 // Create
@@ -19,17 +19,27 @@ app.use("/create", async (req, res) => {
 })
 
 app.use("/read", async (req, res) => {
+  const { id } = req.query
   const db = await getDatabaseInstance()
-  const result = await db.all(`SELECT * FROM movies`);
-  res.send(result);
-});
+  const result = await db.all(`SELECT * FROM movies WHERE id = ?` [id])
+  res.send(result)
+})
+
+app.use("/update", async (req, res) => {
+  const { id, title, source, description, thumb } = req.query
+  const db = await getDatabaseInstance()
+  const result = await db.run(`UPDATE movies SET title=?, source=?, description=?, thumb=? WHERE id=?`, [title, source, description, thumb, id])
+  res.send(result.changes ? "modificado" : "não modificado")
+})
+
 
 app.use("/delete", async (req, res) => {
   const { id } = req.query
   const db = await getDatabaseInstance()
-  await db.run(`DELETE FROM movies WHERE id = ?`, [id])
-  res.send(`Filme deletado.`)
+  const result = await db.run(`DELETE FROM movies WHERE id = ?`, [id])
+  res.send(result.changes ? "deletado" : "não deletado")
 })
 
-const port = 3000;
+
+const port = 3000
 app.listen(port, () => console.log(`Servidor rodando de boa na porta: ${port}.`))
